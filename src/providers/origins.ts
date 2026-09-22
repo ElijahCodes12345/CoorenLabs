75830dc16730a6ddbef7037afed4e85398036261
Aneesh Shukla <130237529+arzeck@users.noreply.github.com>
2026-09-22 16:29:21 +0530

    Update origins.ts

--- src/providers/origins.ts
+++ src/providers/origins.ts
@@ -2,7 +2,7 @@ export const animepahe = "https://animepahe.com";
 export const animekai = "https://anikai.to";
 export const toonstream = "https://toonstream.vip";
 export const animesalt = "https://animesalt.ac";
-export const animelok = "https://animelok.live";
+export const animelok = "https://animelok.cc";
 
 export const flixhq = "https://flixhq.to";
 export const yflix = "https://yflix.to";
